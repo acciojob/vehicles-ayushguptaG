a803b050d84472c056e47ec01cdd6f736fdb4247
Ayush Gupta <ayushgupta068@gmail.com>
2022-12-18 16:29:06 +0530

    added boat class and implemented some methods

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -3,11 +3,13 @@ package com.driver;
 public class F1 extends Car {
 
     public F1(String name, boolean isManual) {
+         super(name, 4,2,7,isManual,"racing",2);
         //Use arbitrary values for parameters which are not mentioned
     }
 
     public void accelerate(int rate){
         int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+        newSpeed= this.getCurrentSpeed()+rate;
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -20,11 +22,29 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            super.stop();
+            this.changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        else if(newSpeed<=50){
+            this.changeGear(1);
+        }
+        else if(newSpeed<=100){
+            this.changeGear(2);
+        }
+        else if(newSpeed <= 150) {
+            this.changeGear(3);
+        }
+        else if(newSpeed<=200){
+            this.changeGear(4);
+        }
+        else if(newSpeed<=250){
+            this.changeGear(5);
+        }
+        else this.changeGear(6);
 
         if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+            changeSpeed(newSpeed, super.getCurrentDirection());
         }
     }
 }
